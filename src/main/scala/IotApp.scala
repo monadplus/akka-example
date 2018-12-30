836f1f04f29aebdb9ac8f4972e74c52f55078d7a
Arnau Abella Gassol <arnauabella@gmail.com>
2018-12-30 21:48:50 +0100

    Fix .gitignore

---
+++ src/main/scala/IotApp.scala
@@ -0,0 +1,18 @@
+import akka.actor.ActorSystem
+import scala.io.StdIn
+
+object IotApp {
+
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("iot-system")
+
+    try {
+      val supervisor = system.actorOf(IotSupervisor.props(), "iot-supervisor")
+      // Exit the system after ENTER is pressed
+      StdIn.readLine()
+    } finally {
+      system.terminate()
+    }
+  }
+
+}
